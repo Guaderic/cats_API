642d1629d719232a12a1b5bca004af4cdb53aa72
Guaderic <VladymyrGolovan@gmail.com>
2022-06-23 22:09:05 +0300

    vf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
 import { CatsComponent } from './components/cats/cats.component';
+import { CatComponent } from './components/cat/cat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainLayoutComponent,
-    CatsComponent
+    CatsComponent,
+    CatComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/index.ts
@@ -0,0 +1,2 @@
+export * from './cat/cat.component'
+export * from './cats/cats.component'
