68aee74d67552c74760742fcda2d8dc4e47ba1a5
Guaderic <VladymyrGolovan@gmail.com>
2022-06-23 21:39:52 +0300

    vf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { CatsComponent } from './components/cats/cats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    CatsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
